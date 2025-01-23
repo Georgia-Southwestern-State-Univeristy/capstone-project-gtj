bbf36bd495fa9c36a3b8c366880fd4b716c1629c
KindmsKetchup <jsparks3@radar.gsw.edu>
2025-01-22 19:41:27 -0500

    added html pages

--- hotels/views.py
+++ hotels/views.py
@@ -1,4 +1,8 @@
+
+from django.shortcuts import render
+def hotels(request):
+	return render(request, 'hotels/home.html')
 from django.shortcuts import HttpResponse
 
-def index(request):
+def hotels(request):
 	return HttpResponse("This is a temporaryu placeholder. Hotel Bookings should show up here")
