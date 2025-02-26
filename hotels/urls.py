865ea0798cedf9581658e629c5a3f1a75fd11693
KindmsKetchup <jsparks3@radar.gsw.edu>
2025-02-25 20:04:06 -0500

    Docker for Database/App

--- hotels/urls.py
+++ hotels/urls.py
@@ -5,6 +5,5 @@ app_name = 'hotels'
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('search-cities/', views.search_cities, name='search_cities') 
-
+    path('search-cities/', views.search_cities, name='search_cities'),
 ]
