2f91df88589c39983a228082f0803dff7a7ac25e
KindmsKetchup <jsparks3@radar.gsw.edu>
2025-03-03 05:44:43 -0500

    Remove destinations

--- destinations/urls.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bcd9793dfd8633c012c777281a8c283da45fbf689bf8a5eed4a0de16f2adb0e8
-size 212
--- destinations/views.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c0699ef40bb7e22c0827d05864a7dee76a8afca8b0eb505950afcd99e06295ac
-size 12600
