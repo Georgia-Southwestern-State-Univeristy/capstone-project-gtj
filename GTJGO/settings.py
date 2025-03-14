a098aee4a8ba777c9164125d07fb01e3ce4e2c40
KindmsKetchup <jsparks3@radar.gsw.edu>
2025-03-14 16:32:55 -0400

    updated settings for google cloud

--- GTJGO/settings.py
+++ GTJGO/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e1b5ba7161928691f10dba30c3b2037ec61ae677c1f4490635b9ca7d15d1a6a
+oid sha256:96e6599c2c9244657e565320ffbeb9891a9c57b46b650884db4efba4b65ce14b
 size 4420
